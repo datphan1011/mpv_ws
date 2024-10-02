b9c8ba1a7266adbe080dfd9acd2f262cd4709bcf
Dat Phan <thanhdat101100@gmail.com>
2024-10-02 15:10:56 +0200

    Add some comments to the client

--- mpv/src/Client.cpp
+++ mpv/src/Client.cpp
@@ -9,37 +9,44 @@
 class Client: public rclcpp::Node {
 public:
     Client() : Node("client_node") {
+        // Initialize the client to call MPVAndStation service
         client_ = this->create_client<my_custom_service::srv::MPVAndStation>("server_service");
 
-        // Create a timer for each message that has been sent
+        // Timer to periodically send requests (every 2 seconds)
         timer_ = this->create_wall_timer(
             std::chrono::seconds(2), std::bind(&Client::send_request, this));
 
+        // Subscription to the MPV control topic
         mpv_control_sub_ = this->create_subscription<std_msgs::msg::String>(
             "mpv_control", 10, std::bind(&Client::command_callback, this, std::placeholders::_1));
 
+        // Subscriptions to left and right height sensors
         left_height_sensor_sub_ = this->create_subscription<std_msgs::msg::Int32>(
             "left_height_sensor", 10, std::bind(&Client::left_height_sensor_callback, this, std::placeholders::_1));
-
         right_height_sensor_sub_ = this->create_subscription<std_msgs::msg::Int32>(
             "right_height_sensor", 10, std::bind(&Client::right_height_sensor_callback, this, std::placeholders::_1));
 
+        // Initial values for command and sensor data
         last_command_ = "";
         last_left_sensor_data_ = 0;
         last_right_sensor_data_ = 0;
     }
 private:
     //class member variables
+    // ROS2 Client and Timer
     rclcpp::Client<my_custom_service::srv::MPVAndStation>::SharedPtr client_;
     rclcpp::TimerBase::SharedPtr timer_;
+
+    // ROS2 Subscriptions
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr mpv_control_sub_;
     rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr left_height_sensor_sub_;
     rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr right_height_sensor_sub_;
     
+    // Variables for storing last received data
     std::string last_command_;
     int32_t last_left_sensor_data_;
     int32_t last_right_sensor_data_;
-    bool data_sent_ = false; // Check if data has already been sent
+    bool data_sent_ = false; // Check if data has been sent?
 
     //Callback function for left height sensor data (corrected logging)
     void left_height_sensor_callback(const std_msgs::msg::Int32::SharedPtr msg) {
@@ -74,12 +81,12 @@ private:
             RCLCPP_ERROR(this->get_logger(), "Server service not available");
             return;
         }
-
+        // Prepare the service request
         auto request = std::make_shared<my_custom_service::srv::MPVAndStation::Request>();
         request->command = last_command_;
         request->left_sensor_data = last_left_sensor_data_;   
         request->right_sensor_data = last_right_sensor_data_;
-
+        // Send the service request and handle the response
         auto future = client_->async_send_request(request);
         try {
             auto response = future.get();
