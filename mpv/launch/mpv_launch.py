d9bd573ad2a4af0f04cd3ccccb967382446117b9
Dat Phan <thanhdat101100@gmail.com>
2024-10-07 17:00:17 +0200

    Create a launch file for this mpv package

---
+++ mpv/launch/mpv_launch.py
@@ -0,0 +1,31 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='mpv',
+            executable='mpv_control_node',
+            output='screen',
+        ),
+        Node(
+            package='mpv',
+            executable='client_node',
+            name='Client',
+        ),
+        Node(
+            package='mpv',
+            executable='height_sensor_mpv_node',
+            name='height_sensor_mpv'
+        ),
+        Node(
+            package='mpv',
+            executable='limit_switch_node',
+            name='limit_switch',
+        ),
+        Node(
+            package='mpv',
+            executable='lock_stepper_node',
+            name='lock_stepper',
+        )
+    ])
