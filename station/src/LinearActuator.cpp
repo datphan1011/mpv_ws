00cd817f808893419928cc95e4c84ba33e621b53
Dat Phan <thanhdat101100@gmail.com>
2024-10-09 10:43:58 +0200

    Adding some comment

--- station/src/LinearActuator.cpp
+++ station/src/LinearActuator.cpp
@@ -13,14 +13,19 @@ public:
     LinearActuator(int left_up_act_pin, int left_down_act_pin, int right_up_act_pin, int right_down_act_pin) 
     : Node("linear_actuator_node"), left_differences(0), right_differences(0){
         // Initialize the node subscription
+        // mpv left sensor
         mpv_left_sensor_sub_ = this->create_subscription<std_msgs::msg::Int32>(
         "left_height_mpv", 10,std::bind(&LinearActuator::mpv_left_sensor_callback, this, std::placeholders::_1));
+        // station left sensor
         station_left_sensor_sub_ = this->create_subscription<std_msgs::msg::Int32>(
         "left_height_station", 10,std::bind(&LinearActuator::station_left_sensor_callback, this, std::placeholders::_1));
+        // mpv right sensor
         mpv_right_sensor_sub_ = this->create_subscription<std_msgs::msg::Int32>(
         "right_height_mpv", 10,std::bind(&LinearActuator::mpv_right_sensor_callback, this, std::placeholders::_1));
+        // station right sensor
         station_right_sensor_sub_ = this->create_subscription<std_msgs::msg::Int32>(
         "right_height_station", 10,std::bind(&LinearActuator::station_right_sensor_callback, this, std::placeholders::_1));
+        // station control
         station_control_sub_ = this->create_subscription<std_msgs::msg::String>(
         "station_control", 10,std::bind(&LinearActuator::station_control_callback, this, std::placeholders::_1));
         // Initialize Pigpio library
