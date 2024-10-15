a7ff70378d605b298cb946a550da2c5558977f94
Dat Phan <thanhdat101100@gmail.com>
2024-10-15 10:58:04 +0200

    Fixed: comment the class to test the dependencies because it can't regcornize the OpenCV library

--- station/src/QRCodeDetection.cpp
+++ station/src/QRCodeDetection.cpp
@@ -1,17 +1,22 @@
-#include <iostream>
 #include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/int32.hpp>
-#include <open
+#include <sensor_msgs/msg/image.hpp>
+#include <std_msgs/msg/header.hpp>
+#include <cv_bridge/cv_bridge.h> // cv_bridge converts between ROS 2 image messages and OpenCV image representations.
+#include <image_transport/image_transport.hpp> // Using image_transport allows us to publish and subscribe to compressed image streams in ROS2
+#include <opencv2/opencv.hpp> // We include everything about OpenCV as we don't care much about compilation time at the moment.
+#include <chrono>
+#include <iostream>
 
-class QRCodeDetection : public rclcpp :: Node{
-public:
-    QRCodeDetection() : Node("qr_detection_node"){
 
-    }
-private:
-};
+// class QRCodeDetection : public rclcpp :: Node{
+// public:
+//     QRCodeDetection() : Node("qr_detection_node"), count_(0){
+//         image_publisher_ = this->create_publisher<sensor_msgs::msg::Image>
+//     }
+// private:
+// };
 
-int main(int argc , char argv**){
+int main(int argc ,char **argv){
     rclcpp::init(argc, argv);
     rclcpp::shutdown();
     return 0;
