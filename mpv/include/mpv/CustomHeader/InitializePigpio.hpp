5f14850686fd6fb861b78824b9cfa96127a9300a
Dat Phan <thanhdat101100@gmail.com>
2024-11-18 13:44:11 +0100

    First commit after modify .gitignore

---
+++ mpv/include/mpv/CustomHeader/InitializePigpio.hpp
@@ -0,0 +1,18 @@
+// pigpio_initializer.hpp
+
+#ifndef PIGPIO_INITIALIZER_HPP
+#define PIGPIO_INITIALIZER_HPP
+
+#include <pigpio.h>
+#include <rclcpp/rclcpp.hpp>
+
+int initialize_PIGPIO(const rclcpp::Logger &logger) {
+    if (gpioInitialise() < 0) {
+        RCLCPP_ERROR(logger, "Failed to initialize pigpio library");
+        return -1;  // Return error code
+    }
+    RCLCPP_INFO(logger, "pigpio library initialized successfully");
+    return 0;  // Success
+}
+
+#endif  // PIGPIO_INITIALIZER_HPP
