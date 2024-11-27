c8dd192f5a3f55bece8fb354a12fc6a78b7d6859
Dat Phan <thanhdat101100@gmail.com>
2024-11-27 16:19:21 +0100

    Update the launch file with some command and output it to the screen terminal

--- station/launch/station_launch.py
+++ station/launch/station_launch.py
@@ -1,48 +1,62 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node 
+from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+        # Server Node
         Node(
-            package = 'station',
-            executable = 'server_node',
-            name = 'Server',
+            package='station',
+            executable='server_node',
+            name='Server',
+            output='screen'
         ),
+        # Station Control Node
         Node(
-            package = 'station',
-            executable = 'station_control_node',
-            name = 'StationControl',
-            output = 'screen',
+            package='station',
+            executable='station_control_node',
+            name='StationControl',
+            output='screen'
         ),
+        # Gripper Node
         Node(
-            package = 'station',
-            executable = 'gripper_node', 
-            name = 'Gripper',
+            package='station',
+            executable='gripper_node',
+            name='Gripper',
+            output='screen'
         ),
+        # Height Sensor Node
         Node(
-            package = 'station',
-            executable = 'height_sensor_node',
-            name = 'HeightSensorStation',
-            output = 'screen',
+            package='station',
+            executable='height_sensor_node',
+            name='HeightSensorStation',
+            output='screen'
         ),
+        # QR Code Detection Node
         Node(
-            package = 'station',
-            name = 'LeadScrew',
-            executable = 'lead_screw_node',
+            package='station',
+            executable='qr_code_node',
+            name='QRCodeDetection',
+            output='screen'
         ),
+        # Lead Screw Node
         Node(
-            package = 'station',
-            executable = 'limitswitch_station_node',
-            name = 'LimitSwitch',
+            package='station',
+            executable='lead_screw_node',
+            name='LeadScrew',
+            output='screen'
         ),
+        # Limit Switch Node
         Node(
-            package = 'station',
-            executable = 'linear_actuator_node',
-            name = 'LinearActuator',
+            package='station',
+            executable='limitswitch_station_node',
+            name='LimitSwitch',
+            output='screen'
         ),
+        # Linear Actuator Node
         Node(
-            package = 'station',
-            executable = 'qr_code_node',
-            name = 'QRCodeDetection',
+            package='station',
+            executable='linear_actuator_node',
+            name='LinearActuator',
+            output='screen'
         ),
     ])
